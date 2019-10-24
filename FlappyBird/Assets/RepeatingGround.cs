4638f44002b3c0c96cda52989ecebf32b9710924
Aylin Görgün <aylingrgn2@gmail.com>
2019-10-24 22:12:51 +0300

    Flappy Bird Clone lab1
    11.10.19

---
+++ FlappyBird/Assets/RepeatingGround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingGround : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnBecameInvisible()
+    {
+        float size = this.gameObject.GetComponent<BoxCollider2D>().size.x;
+        new Vector2(this.gameObject.GetComponent<BoxCollider2D>().size.x * 2, 0);
+        transform.position = (Vector2)this.transform.position + new Vector2(this.gameObject.GetComponent<BoxCollider2D>().size.x * 2, 0);
+
+
+    }
+}
---
+++ FlappyBird/Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BirdMovement : MonoBehaviour
+{
+    public float speed = 2;
+    public float force = 300;
+    // Start is called before the first frame update
+    void Start()
+    {
+       this.GetComponent<Rigidbody2D>().velocity = Vector2.right* speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+            this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}

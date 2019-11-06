ff419fda6523c65947283196940e3695c9baad6f
Aylin Görgün <aylingrgn2@gmail.com>
2019-11-07 01:18:15 +0300

    TileliT added.

---
+++ TileeliT/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    public GameObject platformPrefab;
+
+    //public int numberOfPlatforms = 200;
+    public float levelWidth = 1.5f;
+    public float minY = .5f;
+    public float maxY = 1f;
+
+    Vector3 spawnPosition;
+
+    // Use this for initialization
+    private void Start()
+    {
+        spawnPosition = new Vector3();
+        //int i = 0; i < numberOfPlatforms; i++
+
+    }
+
+    void Update()
+    {
+
+        while (Platform.colliderCheck == true)
+        {
+            spawnPosition.y += Random.Range(minY, maxY);
+            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+
+            Platform.colliderCheck = false;
+        }
+    }
+}
---
+++ TileeliT/Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+
+    public float movementSpeed = 10f;
+    Rigidbody2D rb;
+    float movement = 0f;
+
+    public Transform start;
+    Vector3 st;
+    public Text distance;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        st = start.position;
+
+        distance.GetComponent<Text>().text = "Distance: 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+
+        int dist = (int)Vector3.Distance(st, transform.position);
+        distance.text = "Distance: " + dist;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    }
+
+    /*
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Respawn")
+        {
+            GameObject duplicate = Instantiate(GameObject.FindGameObjectWithTag("Player"));
+        }   
+    }*/
+}

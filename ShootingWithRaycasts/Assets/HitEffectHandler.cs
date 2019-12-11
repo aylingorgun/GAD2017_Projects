c0e7754e36ce8be14d03dc3e79f8fb2ee90c0fc7
Aylin Görgün <aylingrgn2@gmail.com>
2019-12-11 23:25:37 +0300

    Revert "Raycast Shppting Project decreased size."
    This reverts commit 75c91ae9a1c26c1acbf9180a3d70fa4d8b7a4742.

---
+++ ShootingWithRaycasts/Assets/HitEffectHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitEffectHandler : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        StartCoroutine(DeactivateInpactParticle());
+    }
+
+    IEnumerator DeactivateInpactParticle()
+    {
+        yield return new WaitForSeconds(4f);
+        gameObject.SetActive(false);
+    }
+}
